33cc05abaaf951c0d58dee513887912e684fddb2
Mariusrb <>
2020-01-17 08:44:19 +0100

    post test

--- app/src/main/java/com/example/nestenbook/LoginScreen.kt
+++ app/src/main/java/com/example/nestenbook/LoginScreen.kt
@@ -3,8 +3,15 @@ package com.example.nestenbook
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
+import java.net.URLEncoder
 
 class LoginScreen : AppCompatActivity() {
 
@@ -28,7 +35,34 @@ class LoginScreen : AppCompatActivity() {
         if (email.isEmpty() || password.isEmpty()) {
             Toast.makeText(this, "Please enter Email and Password", Toast.LENGTH_SHORT).show()
         } else {
-            TODO("Perform logincheck with database")
+            var reqParam = URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(email, "UTF-8")
+            reqParam += "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8")
+
+            val myURL = URL("https://itstud.hiof.no/~runeei/gru3/")
+
+            with(myURL.openConnection() as HttpURLConnection){
+            requestMethod = "POST"
+                val wr = OutputStreamWriter(outputStream)
+                wr.write(reqParam)
+                wr.flush()
+
+                Log.d("Con", "URL: $url")
+                Log.d("Con", "Responsecode: $responseCode")
+
+
+
+                BufferedReader(InputStreamReader(inputStream)).use {
+                    val response = StringBuffer()
+
+                    var inputLine = it.readLine()
+                    while (inputLine != null){
+                        response.append(inputLine)
+                        inputLine = it.readLine()
+                    }
+                    it.close()
+                    Log.d("Con", "Response: $response")
+                }
+            }
         }
 
     }
--- app/src/main/java/com/example/nestenbook/RegisterScreen.kt
+++ app/src/main/java/com/example/nestenbook/RegisterScreen.kt
@@ -1,13 +1,14 @@
 package com.example.nestenbook
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.AsyncTask
 import android.os.Bundle
-import android.renderscript.Sampler
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_register_screen.*
+
 
 class RegisterScreen : AppCompatActivity() {
 
@@ -26,7 +27,6 @@ class RegisterScreen : AppCompatActivity() {
         spinner.onItemSelectedListener = object :
             AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
-                TODO("Implement if needed")
             }
             override fun onItemSelected(
                 parent: AdapterView<*>,
@@ -40,10 +40,10 @@ class RegisterScreen : AppCompatActivity() {
             val surname = registerSurname.text.toString()
             val email = registerEmail.text.toString()
             val password = registerPassword.text.toString()
-
-            TODO("Send information to database")
+            Log.d("Test", "$name $surname $email $password $course")
         }
     }
 }
+
 
 
